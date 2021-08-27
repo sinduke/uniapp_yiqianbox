755e544203f5da64f820b57965c899e75695e038
PC-20200820RIJS\Administrator <595909297@qq.com>
2021-08-27 11:01:21 +0800

    账号记录，充值中心

--- store/store.js
+++ store/store.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import createPersistedState from 'vuex-persistedstate'  
+// import createPersistedState from 'vuex-persistedstate'  
 Vue.use(Vuex);
 
 import state from "./state.js"
@@ -9,15 +9,15 @@ import mutations from "./mutations.js"
 const store = new Vuex.Store({
 	state,
 	mutations,
-	plugins: [createPersistedState({
-		key: 'app_config_data',  // 状态保存到本地的 key   
-		      paths: ['downTasksList'],  // 要持久化的状态，在state里面取，如果有嵌套，可以  a.b.c   
-		      storage: {  // 存储方式定义  
-		        getItem: (key) => uni.getStorageSync(key), // 获取  
-		        setItem: (key, value) => uni.setStorageSync(key, value), // 存储  
-		        removeItem: (key) => uni.removeStorageSync(key) // 删除  
-				}
-	})]
+	// plugins: [createPersistedState({
+	// 	key: 'app_config_data',  // 状态保存到本地的 key   
+	// 	      paths: ['downTasksList'],  // 要持久化的状态，在state里面取，如果有嵌套，可以  a.b.c   
+	// 	      storage: {  // 存储方式定义  
+	// 	        getItem: (key) => uni.getStorageSync(key), // 获取  
+	// 	        setItem: (key, value) => uni.setStorageSync(key, value), // 存储  
+	// 	        removeItem: (key) => uni.removeStorageSync(key) // 删除  
+	// 			}
+	// })]
 })
 
 export default store
